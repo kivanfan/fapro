3a270cfcdf6b68bcba5e89058b47027f7eb724e5
kivan <kivan011040@gmail.com>
2019-10-17 14:55:46 +0800

    Update to 5.11.2-rc2

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ```
-<link rel="stylesheet" href="/node_modules/fapro/css/all.css">
-<link rel="stylesheet" href="https://unpkg.com/fapro@5.11.2-rc.1/css/all.min.css">
+<link rel="stylesheet" href="/node_modules/fapro/css/all.min.css">
+<link rel="stylesheet" href="https://unpkg.com/fapro@5.11.2-rc.2/css/all.min.css">
 
 ```
 npm i fapro
---
+++ svg/test.php
@@ -0,0 +1,281 @@
+<?php
+$svgs = scandir(".");
+//var_dump($svgs);
+$brand_free_start = '<?xml version="1.0" standalone="no"?>
+<!--
+Font Awesome Pro 5.11.2 by @fontawesome - https://fontawesome.com
+License - https://fontawesome.com/license (Commercial License)
+-->
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd" >
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
+<metadata>
+Created by FontForge 20190801 at Mon Sep 23 12:54:42 2019
+ By Robert Madole
+Copyright (c) Font Awesome
+</metadata>
+<defs>
+<font id="FontAwesome5Brands-Regular" horiz-adv-x="512" >
+  <font-face 
+    font-family="Font Awesome 5 Brands Regular"
+    font-weight="400"
+    font-stretch="normal"
+    units-per-em="512"
+    panose-1="2 0 5 3 0 0 0 0 0 0"
+    ascent="448"
+    descent="-64"
+    bbox="7.33008 -64 504.641 448.002"
+    underline-thickness="25"
+    underline-position="-50"
+    unicode-range="U+0020-F8E8"
+  />
+    <missing-glyph />';
+$duotone_pro_start = '<?xml version="1.0" standalone="no"?>
+<!--
+Font Awesome Pro 5.11.2 by @fontawesome - https://fontawesome.com
+License - https://fontawesome.com/license (Commercial License)
+-->
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd" >
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
+<metadata>
+Created by FontForge 20190801 at Mon Sep 23 12:54:53 2019
+ By Robert Madole
+Copyright (c) Font Awesome
+</metadata>
+<defs>
+<font id="FontAwesome5Duotone-Solid" horiz-adv-x="640" >
+  <font-face 
+    font-family="Font Awesome 5 Duotone Solid"
+    font-weight="900"
+    font-stretch="normal"
+    units-per-em="512"
+    panose-1="2 0 5 3 0 0 0 0 0 0"
+    ascent="448"
+    descent="-64"
+    bbox="0 -64 640 448"
+    underline-thickness="25"
+    underline-position="-50"
+    unicode-range="U+0020-10F8F3"
+  />
+    <missing-glyph />';
+$light_pro_start = '<?xml version="1.0" standalone="no"?>
+<!--
+Font Awesome Pro 5.11.2 by @fontawesome - https://fontawesome.com
+License - https://fontawesome.com/license (Commercial License)
+-->
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd" >
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
+<metadata>
+Created by FontForge 20190801 at Mon Sep 23 12:54:50 2019
+ By Robert Madole
+Copyright (c) Font Awesome
+</metadata>
+<defs>
+<font id="FontAwesome5Pro-Light" horiz-adv-x="640" >
+  <font-face 
+    font-family="Font Awesome 5 Pro Light"
+    font-weight="300"
+    font-stretch="normal"
+    units-per-em="512"
+    panose-1="2 0 3 3 0 0 0 0 0 0"
+    ascent="448"
+    descent="-64"
+    bbox="-0.0602614 -64 640 448"
+    underline-thickness="25"
+    underline-position="-50"
+    unicode-range="U+0020-F8F3"
+  />
+    <missing-glyph />';
+$regular_free_start = '<?xml version="1.0" standalone="no"?>
+<!--
+Font Awesome Pro 5.11.1 by @fontawesome - https://fontawesome.com
+License - https://fontawesome.com/license (Commercial License)
+-->
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd" >
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
+<metadata>
+Created by FontForge 20190801 at Wed Sep 18 17:46:55 2019
+ By Robert Madole
+Copyright (c) Font Awesome
+</metadata>
+<defs>
+<font id="FontAwesome5Pro-Regular" horiz-adv-x="496" >
+  <font-face 
+    font-family="Font Awesome 5 Pro Regular"
+    font-weight="400"
+    font-stretch="normal"
+    units-per-em="512"
+    panose-1="2 0 5 3 0 0 0 0 0 0"
+    ascent="448"
+    descent="-64"
+    bbox="0 -64 503.994 448"
+    underline-thickness="25"
+    underline-position="-50"
+    unicode-range="U+0020-F8F2"
+  />
+    <missing-glyph />';
+$regular_pro_start = '<?xml version="1.0" standalone="no"?>
+<!--
+Font Awesome Pro 5.11.2 by @fontawesome - https://fontawesome.com
+License - https://fontawesome.com/license (Commercial License)
+-->
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd" >
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
+<metadata>
+Created by FontForge 20190801 at Mon Sep 23 12:54:55 2019
+ By Robert Madole
+Copyright (c) Font Awesome
+</metadata>
+<defs>
+<font id="FontAwesome5Pro-Regular" horiz-adv-x="640" >
+  <font-face 
+    font-family="Font Awesome 5 Pro Regular"
+    font-weight="400"
+    font-stretch="normal"
+    units-per-em="512"
+    panose-1="2 0 5 3 0 0 0 0 0 0"
+    ascent="448"
+    descent="-64"
+    bbox="-0.0602614 -64 640 448"
+    underline-thickness="25"
+    underline-position="-50"
+    unicode-range="U+0020-F8F3"
+  />
+    <missing-glyph />';
+$solid_free_start = '<?xml version="1.0" standalone="no"?>
+<!--
+Font Awesome Pro 5.11.1 by @fontawesome - https://fontawesome.com
+License - https://fontawesome.com/license (Commercial License)
+-->
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd" >
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
+<metadata>
+Created by FontForge 20190801 at Wed Sep 18 17:46:55 2019
+ By Robert Madole
+Copyright (c) Font Awesome
+</metadata>
+<defs>
+<font id="FontAwesome5Pro-Solid" horiz-adv-x="496" >
+  <font-face 
+    font-family="Font Awesome 5 Pro Solid"
+    font-weight="900"
+    font-stretch="normal"
+    units-per-em="512"
+    panose-1="2 0 5 3 0 0 0 0 0 0"
+    ascent="448"
+    descent="-64"
+    bbox="-0.0328251 -64.0039 640.001 448.427"
+    underline-thickness="25"
+    underline-position="-50"
+    unicode-range="U+0020-F8F2"
+  />
+    <missing-glyph />';
+$solid_pro_start = '<?xml version="1.0" standalone="no"?>
+<!--
+Font Awesome Pro 5.11.2 by @fontawesome - https://fontawesome.com
+License - https://fontawesome.com/license (Commercial License)
+-->
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd" >
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
+<metadata>
+Created by FontForge 20190801 at Mon Sep 23 12:54:55 2019
+ By Robert Madole
+Copyright (c) Font Awesome
+</metadata>
+<defs>
+<font id="FontAwesome5Pro-Solid" horiz-adv-x="640" >
+  <font-face 
+    font-family="Font Awesome 5 Pro Solid"
+    font-weight="900"
+    font-stretch="normal"
+    units-per-em="512"
+    panose-1="2 0 5 3 0 0 0 0 0 0"
+    ascent="448"
+    descent="-64"
+    bbox="-0.0602614 -64.0603 640 448"
+    underline-thickness="25"
+    underline-position="-50"
+    unicode-range="U+0020-F8F3"
+  />
+    <missing-glyph />';
+$end = '  </font>
+</defs></svg>';
+$brand_free = [];
+$duotone_pro = [];
+$light_pro = [];
+$regular_free = [];
+$regular_pro = [];
+$solid_free = [];
+$solid_pro = [];
+$count="";
+foreach ($svgs as $svg) {
+    $file = file_get_contents($svg);
+    if (strpos($svg, 'fa-brands-400-free') === 0) {
+        preg_match_all("/<glyph[\s\S]*?[>]/", $file, $glyArr);
+        $brand_free = array_merge($brand_free, $glyArr[0]);
+    }
+
+    if (strpos($svg, 'fa-duotone-900-pro') === 0) {
+        preg_match_all("/<glyph[\s\S]*?[>]/", $file, $glyArr);
+        $duotone_pro = array_merge($duotone_pro, $glyArr[0]);
+    }
+
+    if (strpos($svg, 'fa-light-300-pro') === 0) {
+        preg_match_all("/<glyph[\s\S]*?[>]/", $file, $glyArr);
+        $light_pro = array_merge($light_pro, $glyArr[0]);
+    }
+
+    if (strpos($svg, 'fa-regular-400-free') === 0) {
+        preg_match_all("/<glyph[\s\S]*?[>]/", $file, $glyArr);
+        $regular_free = array_merge($regular_free, $glyArr[0]);
+    }
+
+    if (strpos($svg, 'fa-regular-400-pro') === 0) {
+        preg_match_all("/<glyph[\s\S]*?[>]/", $file, $glyArr);
+        $regular_pro = array_merge($regular_pro, $glyArr[0]);
+    }
+
+    if (strpos($svg, 'fa-solid-900-free') === 0) {
+        preg_match_all("/<glyph[\s\S]*?[>]/", $file, $glyArr);
+        $solid_free = array_merge($solid_free, $glyArr[0]);
+    }
+
+    if (strpos($svg, 'fa-solid-900-pro') === 0) {
+        preg_match_all("/<glyph[\s\S]*?[>]/", $file, $glyArr);
+        $solid_pro = array_merge($solid_pro, $glyArr[0]);
+    }
+    $count++;
+}
+//var_dump($duotone_pro);
+$brand_free = $brand_free_start . implode("", $brand_free) . $end;
+$duotone_pro = $duotone_pro_start . implode("", $duotone_pro) . $end;
+$light_pro = $light_pro_start . implode("", $light_pro) . $end;
+$regular_free = $regular_free_start . implode("", $regular_free) . $end;
+$regular_pro = $regular_pro_start . implode("", $regular_pro) . $end;
+$solid_free = $solid_free_start . implode("", $solid_free) . $end;
+$solid_pro = $solid_pro_start . implode("", $solid_pro) . $end;
+
+
+$brand_free_file = fopen("fa-brands-400-free.svg", "w");
+$duotone_pro_file = fopen("fa-duotone-900-pro.svg", "w");
+$light_pro_file = fopen("fa-light-300-pro.svg", "w");
+$regular_free_file = fopen("fa-regular-400-free.svg", "w");
+$regular_pro_file = fopen("fa-regular-400-pro.svg", "w");
+$solid_free_file = fopen("fa-solid-900-free.svg", "w");
+$solid_pro_file = fopen("fa-solid-900-pro.svg", "w");
+
+fwrite($brand_free_file, $brand_free);
+fwrite($duotone_pro_file, $duotone_pro);
+fwrite($light_pro_file, $light_pro);
+fwrite($regular_free_file, $regular_free);
+fwrite($regular_pro_file, $regular_pro);
+fwrite($solid_free_file, $solid_free);
+fwrite($solid_pro_file, $solid_pro);
+
+fclose($brand_free_file);
+fclose($duotone_pro_file);
+fclose($light_pro_file);
+fclose($regular_free_file);
+fclose($regular_pro_file);
+fclose($solid_free_file);
+fclose($solid_pro_file);
+
